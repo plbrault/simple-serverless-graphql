b4a438cae627291b27d14eed1c704ae0d00bdf96
Pier-Luc Brault <plbrault@gmail.com>
2016-10-05 23:09:27 -0400

    Add project boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: [
+  ],
+  parser: 'babel-eslint',
+  rules: {}
+};
---
+++ src/handler.js
@@ -0,0 +1,3 @@
+export default (event, context, cb) => {
+  cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
